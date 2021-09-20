64e7fe3b6dfc2c17cbdf345f8243fa22ee478c4d
AndersonCRocha <dan.sjose20@gmail.com>
2021-09-20 20:30:42 -0300

    Migrando endpoint para arquivo de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import router from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request: Request, response: Response) => response.send('Hello world!'));
+app.use(router);
 
 const port = process.env.PORT || 3000;
 
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+router.get('/', (request: Request, response: Response) => response.send('Hello World!'));
+
+export default router;
