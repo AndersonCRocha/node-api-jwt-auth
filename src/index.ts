f5178dd440cd713bf21cc103af1ecb2cf745ca9c
AndersonCRocha <dan.sjose20@gmail.com>
2021-09-20 20:23:13 -0300

    Configurando eslint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import express, { Request, Response }from 'express'
+import express, { Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request: Request, response: Response) => {
-  return response.send('Hello world!')
-})
+app.get('/', (request: Request, response: Response) => response.send('Hello world!'));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running in http://localhost:${port}`))
+app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
