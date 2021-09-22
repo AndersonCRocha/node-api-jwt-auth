e1c2636385eb97baefbea4b1d1dae38c8545f1da
AndersonCRocha <dan.sjose20@gmail.com>
2021-09-22 20:28:44 -0300

    Alterando projeto para utilizar o ES2018

--- src/app/utils/module-alias.ts
+++ src/app/utils/module-alias.ts
@@ -4,6 +4,7 @@ import moduleAlias from 'module-alias';
 const files = path.resolve(__dirname, '../../../');
 
 moduleAlias.addAliases({
+  '@': path.join(files, 'src'),
   '@models': path.join(files, 'src', 'app', 'models'),
   '@controllers': path.join(files, 'src', 'app', 'controllers'),
 });
